93bb6ffa55339baefd84b662ab3b3c5ac4cc9d98
Иван <ivan.vergeichik@mail.ru>
2023-05-26 01:42:24 +0300

    Создание кнопки входа

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ButtonsScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ButtonsScreen.kt
@@ -1,14 +1,25 @@
 package com.raywenderlich.android.lab1.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
+import org.w3c.dom.Text
 
 @Composable
 fun ExploreButtonsScreen() {
@@ -28,8 +39,22 @@ fun ExploreButtonsScreen() {
 
 @Composable
 fun MyButton() {
-    //TODO add your code here
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.blue)),
+        border = BorderStroke(
+            1.dp,
+            color = colorResource(id = R.color.black)
+        )
+    ) {
+        Text(
+            text = stringResource(id = R.string.set_button),
+            color = Color.White
+        )
+    }
 }
+    //TODO add your code here
+
 
 @Composable
 fun MyRadioGroup() {
